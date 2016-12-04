88af053c4cc47d7323ca91f987f70e36d0e6c3c8
bilel <bilel.kedidi@gmail.com>
2016-12-04 19:32:06 +0100

    add ws autho

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   #ACTION CABLE
-  config.action_cable.allowed_request_origins = [ 'localhost:3000', 'cms.microhealthllc.com', /http:\/\/localhost:*/ ]
+  config.action_cable.allowed_request_origins = [ 'localhost:3000', 'cms.microhealthllc.com', /http:\/\/localhost:*/, /https:\/\/cms.microhealthllc.com/]
 
 
   config.action_mailer.delivery_method = :letter_opener
