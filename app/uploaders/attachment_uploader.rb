327c1878296ddba61adccd1d6efcabb71ebbd6d4
bilel-kedidi <bilel.kedidi@gmail.com>
2016-12-06 14:07:11 +0100

    Update attachment_uploader.rb

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -40,7 +40,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(jpg jpeg gif png pdf doc dox xls xlsx csv txt)
+    %w(jpg jpeg gif png pdf doc docx xls xlsx csv txt)
   end
 
   # Override the filename of the uploaded files:
