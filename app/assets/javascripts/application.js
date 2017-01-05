00fd13b6cdb176fdfd4f9582367b29ad443a6c7c
bilel.kedidi <bilel@classnco.com>
2017-01-05 09:54:29 +0100

    add datatable to case page table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -64,6 +64,22 @@ function createCookie(name,value,days) {
     document.cookie = name+"="+value+expires+"; path=/";
 }
 
+function initDataTable(table_id)
+{
+    $('#'+table_id).DataTable({
+        display: [[0, 'desc']],
+        "bDestroy": true,
+        "sDom": "<'dt-toolbar'" +
+        "<'col-sm-5 col-xs-8'f>" + //search box
+        "<'col-sm-2 col-sm-offset-4 col-xs-2 'C>"+// drop down size
+        "<'col-sm-1 col-xs-2 'l>>"+// drop down size
+        "t"+ // the table
+        "<'dt-toolbar-footer'<'col-sm-6 col-xs-12 hidden-xs'i><'col-xs-12 col-sm-6'p>>",
+        "iDisplayLength": 10
+
+    });
+}
+
 function readCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
