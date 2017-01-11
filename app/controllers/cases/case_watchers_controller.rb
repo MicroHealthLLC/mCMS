0a6809f97047d8429f09e285fd2c298a1a3a37a1
bilel.kedidi <bilel@classnco.com>
2017-01-11 15:36:31 +0100

    fix appointments

--- app/controllers/cases/case_watchers_controller.rb
+++ app/controllers/cases/case_watchers_controller.rb
@@ -3,7 +3,9 @@ class CaseWatchersController < UserCasesController
   # GET /case_watchers
   # GET /case_watchers.json
   def index
-    @case_watchers = CaseWatcher.visible.
+    case_ids = Case.root.where('assigned_to_id= ? OR user_id= ?', User.current.id,  User.current.id ).pluck(:id)
+
+    @case_watchers = CaseWatcher.where(case_id: case_ids).
         includes(:case).paginate(page: params[:page], per_page: 25)
   end
 end
