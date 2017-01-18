9bb3e3d9a0446b49488ed5967a7cce11fbbad850
bilel.kedidi <bilel@classnco.com>
2017-01-18 15:10:03 +0100

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,6 +73,9 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
+    if where(email: auth.info.email || "#{auth.uid}@#{auth.provider}.com").present?
+      return where(email: auth.info.email || "#{auth.uid}@#{auth.provider}.com").first
+    end
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
