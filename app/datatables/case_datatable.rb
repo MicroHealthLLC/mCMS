d071b9f2738da7870c5f52c69e9ac0d44d233a53
bilel.kedidi <bilel@classnco.com>
2017-01-24 15:52:50 +0100

    fixes

--- app/datatables/case_datatable.rb
+++ app/datatables/case_datatable.rb
@@ -22,24 +22,60 @@ class CaseDatatable < AjaxDatatablesRails::Base
     @searchable_columns ||= %w{
     Case.title
     Enumeration.name
-
+    Enumeration.name
+    Enumeration.name
+    Enumeration.name
+    Case.date_start
+    Case.date_due
+    Case.date_completed
     }
   end
 
   private
-
+  # <% @cases.each do |c| %>
+  #         <tr>
+  # <% if User.current.can?(:manage_roles) and params[:my] %>
+  #     <td><%= link_to_user(c.user) %></td>
+  #           <% end %>
+  #           <td><%= link_to(c.title, case_path(c) ) %></td>
+  #           <td><%= c.priority_type %></td>
+  #     <td><%= c.case_type %></td>
+  #           <td><%= c.case_status_type %></td>
+  #     <td><%= c.case_category_type %></td>
+  #           <td><%= c.date_start %></td>
+  #     <td><%= c.date_due %></td>
+  #           <td><%= c.date_completed %></td>
+  #     </tr>
+  #     <% end %>
   def data
-    records.map do |c|
-      [
-          @view.link_to(c.title, @view.case_path(c) ),
-          c.priority_type.to_s,
-          c.case_type.to_s,
-          c.case_status_type.to_s,
-          c.case_category_type.to_s,
-          c.date_start,
-          c.date_due,
-          c.date_completed
-      ]
+    if User.current.can?(:manage_roles)
+      records.map do |c|
+        [
+            @view.link_to(c.title, @view.case_path(c) ),
+            @view.link_to(c.title, @view.case_path(c) ),
+            c.priority_type.to_s,
+            c.case_type.to_s,
+            c.case_status_type.to_s,
+            c.case_category_type.to_s,
+            c.date_start,
+            c.date_due,
+            c.date_completed
+        ]
+      end
+    else
+      records.map do |c|
+        [
+            @view.link_to(c.title, @view.case_path(c) ),
+            c.priority_type.to_s,
+            c.case_type.to_s,
+            c.case_status_type.to_s,
+            c.case_category_type.to_s,
+            c.date_start,
+            c.date_due,
+            c.date_completed
+        ]
+      end
+
     end
   end
 
