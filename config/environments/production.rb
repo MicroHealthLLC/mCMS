e659724488968b20f8069ac29bf12bd6a5b2dd2b
bilel.kedidi <bilel@classnco.com>
2017-01-30 18:04:36 +0100

    fix chat room websocket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
-  config.action_cable.allowed_request_origins = [ 'localhost:3000', '*.microhealthllc.com', /https:\/\/*.microhealthllc.com/, /http:\/\/localhost:*/ ]
+  config.action_cable.allowed_request_origins = [ 'localhost:3000', 'umed.microhealthllc.com', /https:\/\/umed.microhealthllc.com/, /http:\/\/localhost:*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
