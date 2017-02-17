515b822aaa4a550c8dda56b7962e1b6ac79617a8
bilel <bilel.kedidi@gmail.com>
2017-02-18 00:04:59 +0100

    socioeconomic

--- app/controllers/idcdata_controller.rb
+++ app/controllers/idcdata_controller.rb
@@ -9,7 +9,8 @@ class IdcdataController < ApplicationController
       cond_name<< "name LIKE '%#{s}%'"
     end
     scope = Icd10datum
-    scope = scope.between('Z00', 'Z99') if params[:from]
+    # scope = scope.between('Z00', 'Z99') if params[:from] == 'z00'
+    # scope = scope.between('Z55', 'Z65') if params[:from] == 'z55'
     data = scope.where("((#{cond_name.join(' OR ')} )) OR (#{cond_desc.join(' AND ')} )").
         select("id AS id, CONCAT('(', name,') ', description ) AS value,  CONCAT('(', name,') ', description )  AS label").to_json
 
--- app/models/socioeconomic.rb
+++ app/models/socioeconomic.rb
@@ -1,5 +1,37 @@
 class Socioeconomic < ApplicationRecord
+  belongs_to :user
+  belongs_to :icdcm_code, :foreign_key => 'icdcm_code_id', class_name: 'Icd10datum'
+  belongs_to :socioeconomic_type, optional: true
+  belongs_to :socioeconomic_status, optional: true
+
+  has_many :socioeconomic_attachments, foreign_key: :owner_id, dependent: :destroy
+  accepts_nested_attributes_for :socioeconomic_attachments, reject_if: :all_blank, allow_destroy: true
+
+  validates_presence_of :user_id, :icdcm_code_id
+
+  def socioeconomic_type
+    if socioeconomic_type_id
+      super
+    else
+      SocioeconomicType.default
+    end
+  end
+
+  def socioeconomic_status
+    if socioeconomic_status_id
+      super
+    else
+      SocioeconomicStatus.default
+    end
+  end
+
+
+  def to_s
+    icdcm_code
+  end
+
   def self.safe_attributes
-[:user_id, :icdcm_code_id, :date_identified, :date_resolved, :socioeconomic_status_id, :socioeconomic_type_id, :description]
+    [:user_id, :icdcm_code_id, :date_identified, :date_resolved,
+     :socioeconomic_status_id, :socioeconomic_type_id, :description]
   end
 end
