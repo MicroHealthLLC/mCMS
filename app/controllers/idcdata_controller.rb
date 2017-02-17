9abd013d7b643d416e1ad3e0212c450534eaf596
bilel <bilel.kedidi@gmail.com>
2017-02-17 23:10:31 +0100

    add medicals

--- app/controllers/idcdata_controller.rb
+++ app/controllers/idcdata_controller.rb
@@ -1,9 +1,16 @@
 class IdcdataController < ApplicationController
   def index
     q = params[:term]
+    search = q.to_s.split(' ')
+    cond_desc = []
+    cond_name = []
+    search.each do |s|
+      cond_desc<< "description LIKE '%#{s}%'"
+      cond_name<< "name LIKE '%#{s}%'"
+    end
     scope = Icd10datum
     scope = scope.between('Z00', 'Z99') if params[:from]
-    data = scope.where('(name LIKE ?) OR (description LIKE ?)', "#{q}%", "%#{q}%").
+    data = scope.where("((#{cond_name.join(' OR ')} )) OR (#{cond_desc.join(' AND ')} )").
         select("id AS id, CONCAT('(', name,') ', description ) AS value,  CONCAT('(', name,') ', description )  AS label").to_json
 
     render json: data
--- app/models/medical.rb
+++ app/models/medical.rb
@@ -1,5 +1,39 @@
 class Medical < ApplicationRecord
+  belongs_to :user
+  belongs_to :icdcm_code, :foreign_key => 'icdcm_code_id', class_name: 'Icd10datum'
+  belongs_to :medical_history_type, optional: true
+  belongs_to :medical_history_status, optional: true
+
+  has_many :medical_attachments, foreign_key: :owner_id, dependent: :destroy
+  accepts_nested_attributes_for :medical_attachments, reject_if: :all_blank, allow_destroy: true
+
+  validates_presence_of :user_id, :icdcm_code_id
+
+  def medical_history_type
+    if medical_history_type_id
+      super
+    else
+      MedicalHistoryType.default
+    end
+  end
+
+  def medical_history_status
+    if medical_history_status_id
+      super
+    else
+      MedicalHistoryStatus.default
+    end
+  end
+
+  def to_s
+    icdcm_code
+  end
+
   def self.safe_attributes
-    [:user_id, :icdcm_code_id, :medical_facility, :date_of_diagnosis, :medical_history_status_id, :medical_history_type_id, :description]
+    [:user_id, :icdcm_code_id, :medical_facility, :date_of_diagnosis,
+     :medical_history_status_id, :medical_history_type_id, :description,
+     medical_attachments_attributes: [Attachment.safe_attributes]
+
+    ]
   end
 end
