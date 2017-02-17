53921707c58590f5a0b8d1693f678ee6f02c1532
bilel <bilel.kedidi@gmail.com>
2017-02-17 23:27:37 +0100

    add behariola risk

--- app/models/behavioral_risk.rb
+++ app/models/behavioral_risk.rb
@@ -1,6 +1,38 @@
 class BehavioralRisk < ApplicationRecord
+  belongs_to :user
+  belongs_to :icdcm_code, :foreign_key => 'icdcm_code_id', class_name: 'Icd10datum'
+  belongs_to :behavioral_risk_type, optional: true
+  belongs_to :behavioral_risk_status, optional: true
+
+  has_many :behavioral_risk_attachments, foreign_key: :owner_id, dependent: :destroy
+  accepts_nested_attributes_for :behavioral_risk_attachments, reject_if: :all_blank, allow_destroy: true
+
+  validates_presence_of :user_id, :icdcm_code_id
+
+  def behavioral_risk_type
+    if behavioral_risk_type_id
+      super
+    else
+      BehavioralRiskType.default
+    end
+  end
+
+  def behavioral_risk_status
+    if behavioral_risk_status_id
+      super
+    else
+      BehavioralRiskStatus.default
+    end
+  end
+
+
+  def to_s
+    icdcm_code
+  end
+
   def self.safe_attributes
     [:user_id, :icdcm_code_id, :date_started, :date_ended,
-     :behavioral_risk_status_id, :behavioral_risk_type_id, :description]
+     :behavioral_risk_status_id, :behavioral_risk_type_id, :description,
+     behavioral_risk_attachments_attributes: [Attachment.safe_attributes]]
   end
 end
