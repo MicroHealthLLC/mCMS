456335d77968f4b17cbfbfff651fe08d602b7313
bilel <bilel.kedidi@gmail.com>
2017-02-17 22:29:40 +0100

    fixes

--- app/controllers/idcdata_controller.rb
+++ app/controllers/idcdata_controller.rb
@@ -1,7 +1,8 @@
 class IdcdataController < ApplicationController
   def index
     q = params[:term]
-    scope = Icd10datum.between('Z00', 'Z99') if params[:from]
+    scope = Icd10datum
+    scope = scope.between('Z00', 'Z99') if params[:from]
     data = scope.where('name LIKE ?', "#{q}%").
         select("id AS id, CONCAT('(', name,') ', description ) AS value,  CONCAT('(', name,') ', description )  AS label").to_json
 
