eb6f4b3d6bba9d9694b976ef4e3f5e1bc5999d60
bilel.kedidi <bilel@classnco.com>
2017-02-21 15:28:51 +0100

    fix searching in Cvx

--- app/controllers/immunization_cvx_controller.rb
+++ app/controllers/immunization_cvx_controller.rb
@@ -3,14 +3,16 @@ class ImmunizationCvxController < ApplicationController
     q = params[:term]
     search = q.to_s.split(' ')
     cond_desc = []
+    cond_long_desc = []
     cond_name = []
     search.each do |s|
       cond_desc<< "cvx_short_description LIKE '%#{s}%'"
+      cond_long_desc<< "full_vaccine_name LIKE '%#{s}%'"
       cond_name<< "cvx_code LIKE '%#{s}%'"
     end
     scope = ImmunizationCvx
-    data = scope.where("((#{cond_name.join(' OR ')} )) OR (#{cond_desc.join(' AND ')} )").
-        select("id AS id, CONCAT('(', cvx_code,') ', cvx_short_description ) AS value,  CONCAT('(', cvx_code,') ', cvx_short_description )  AS label").to_json
+    data = scope.where("((#{cond_name.join(' OR ')} )) OR (#{cond_desc.join(' AND ')} ) OR (#{cond_long_desc.join(' AND ')} )").
+        select("id AS id, CONCAT('(', cvx_code,') ', cvx_short_description ) AS value,  CONCAT('(', cvx_code,') ', full_vaccine_name )  AS label").to_json
 
     render json: data
   end
