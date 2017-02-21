abd3b847c6b472235f3fe699d7037289e739b7a5
bilel.kedidi <bilel@classnco.com>
2017-02-21 15:08:49 +0100

    fixes

--- app/models/immunization_cvx.rb
+++ app/models/immunization_cvx.rb
@@ -1,5 +1,5 @@
 class ImmunizationCvx < ApplicationRecord
   def to_s
-    "#{cvx_code} - #{short_description} - #{vaccinestatus}"
+    "#{cvx_code} - #{cvx_short_description} - #{vaccinestatus}"
   end
 end
