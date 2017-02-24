976d4b0ad1ccb2edaab6a9bd88891f786180fe27
bilel.kedidi <bilel@classnco.com>
2017-02-24 15:20:21 +0100

    fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( smart_admin/* chat.js sticky.css, signature.js )
+Rails.application.config.assets.precompile += %w( smart_admin/* chat.js sticky.css signature.js lightbox2.css lightbox2.js )
