22d8c99d604a66ac1bb546b19180daf0a8539c95
bilel.kedidi <bilel@classnco.com>
2017-03-01 16:01:55 +0100

    fix getting user from organization

--- db/migrate/20170301145409_change_action_dates.rb
+++ db/migrate/20170301145409_change_action_dates.rb
@@ -1,4 +1,11 @@
 class ChangeActionDates < ActiveRecord::Migration[5.0]
-  def change
+  def self.up
+    change_column :tasks, :date_start, :datetime
+    change_column :tasks, :date_completed, :datetime
+  end
+
+  def self.down
+    change_column :tasks, :date_start, :date
+    change_column :tasks, :date_completed, :date
   end
 end
