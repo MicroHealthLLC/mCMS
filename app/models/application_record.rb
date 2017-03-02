93ffd717e36a221891f123708eabbf12710458a2
bilel.kedidi <bilel@classnco.com>
2017-03-02 14:21:37 +0100

    refactoring method to get statuses quikly

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -12,4 +12,29 @@ class ApplicationRecord < ActiveRecord::Base
     self.try(:user) == User.current
   end
 
+  # type should be [
+  #    ["CaseStatusType", "case_status_type_id", {:is_closed=>true}], ...
+  # ]
+
+  # example:  Case.include_enumerations.join_enumeration(types)
+  def self.join_enumeration(types = [])
+    scope = self
+    cond = ""
+    cond_where = []
+    cond_where2 = []
+    types.each_with_index do |t, idx|
+      enumeration_type = t[0]
+      enumeration_column = t[1]
+      hash = t[2]
+      cond<< " LEFT JOIN enumerations e#{idx} ON #{self.table_name}.#{enumeration_column} = e#{idx}.id AND e#{idx}.type = '#{enumeration_type}' "
+      if hash
+        hash.each do |k, v|
+          cond_where << "e#{idx}.#{k} = ? "
+          cond_where2 << v
+        end
+      end
+    end
+    scope.joins(cond).where(cond_where.join(' AND '), cond_where2 )
+  end
+
 end
