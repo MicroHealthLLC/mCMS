5194a3803a60f068a834fed748f29fc80218bae4
bilel <bilel.kedidi@gmail.com>
2017-03-11 22:04:25 +0100

    overview migration

--- db/migrate/20170311194333_add_record_to_module.rb
+++ db/migrate/20170311194333_add_record_to_module.rb
@@ -1,4 +1,8 @@
 class AddRecordToModule < ActiveRecord::Migration[5.0]
   def change
+    modules = ['profile_record', 'occupational_record', 'medical_record', 'socioeconomic_record']
+    modules.each do |m|
+      EnabledModule.where(name: m).first_or_create
+    end
   end
 end
