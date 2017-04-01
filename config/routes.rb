3e7ad142b145a567ca57f8be7dd7d9b280e7fdd0
bilel <bilel.kedidi@gmail.com>
2017-04-01 22:32:40 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -66,12 +66,6 @@ Rails.application.routes.draw do
   resources :legals
   resources :financials
   resources :transportations
-  resources :mtf_hospitals
-  resources :incident_histories
-  resources :deployment_histories
-  resources :units
-  resources :awards
-  resources :service_histories
   resources :housings
   resources :health_care_facilities
   resources :admissions
