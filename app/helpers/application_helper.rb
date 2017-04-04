3441e6515ebb9966481cf12759baccca44dfbed4
bilel <bilel.kedidi@gmail.com>
2017-04-04 11:37:22 +0100

    fix: when recaptcha enabled and try to login got an error for enabled_modules

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -245,6 +245,7 @@ module ApplicationHelper
   end
 
   def module_enabled?(module_name)
+    @enabled_modules = EnabledModule.active.pluck(:name).to_set if @enabled_modules.nil?
     @enabled_modules.include?(module_name)
   end
 
