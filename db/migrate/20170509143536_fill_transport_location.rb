47fd692ae5184fca380dc263b5543cbc70fef632
bilel <bilel.kedidi@gmail.com>
2017-05-09 17:23:09 +0100

    migration

--- db/migrate/20170509143536_fill_transport_location.rb
+++ db/migrate/20170509143536_fill_transport_location.rb
@@ -1,4 +1,11 @@
 class FillTransportLocation < ActiveRecord::Migration[5.0]
   def change
+    ['Departing', 'Arriving', 'Return Departure', 'Return Arrival'].each do |name|
+      TransportLocation.create(name: name)
+    end
+
+    add_column :transports, :case_id, :integer
+    add_index :transports, :case_id
+
   end
 end
