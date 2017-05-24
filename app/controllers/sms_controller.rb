933b2b6da4f31094ed2dfd0b0aac56bf7dd7a57d
bilel <bilel.kedidi@gmail.com>
2017-05-24 18:51:41 +0100

    fix sending sms

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -8,11 +8,14 @@ class SmsController < ProtectForgeryApplication
     phone_number = params[:phone_number]
     content= params[:body]
     if phone_number.present? and content.present?
-      UserTexter.send_sms(phone_number, content).deliver_later
+      UserTexter.send_sms(phone_number, content).deliver_now
       flash[:notice] = "SMS sent to #{phone_number}"
     else
       flash[:error] = "Cannot send the sms."
     end
+    redirect_to :back
+  rescue
+    flash[:error] = "Cannot send the sms."
     redirect_to :back
   end
 end
