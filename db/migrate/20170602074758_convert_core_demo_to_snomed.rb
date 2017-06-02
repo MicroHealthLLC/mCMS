340e82c070070caeb81e4bbcf574dd4c19a0a8b1
bilel <bilel.kedidi@gmail.com>
2017-06-02 09:27:13 +0100

    change to snomed

--- db/migrate/20170602074758_convert_core_demo_to_snomed.rb
+++ db/migrate/20170602074758_convert_core_demo_to_snomed.rb
@@ -1,4 +1,17 @@
 class ConvertCoreDemoToSnomed < ActiveRecord::Migration[5.0]
-  def change
+  def self.up
+    add_column :core_demographics, :ethnicity, :string
+    add_column :core_demographics, :religion, :string
+
+    CoreDemographic.all.each do |core_demographic|
+      core_demographic.religion = core_demographic.religion_type.to_s
+      core_demographic.ethnicity = core_demographic.ethnicity_type.to_s
+      core_demographic.save
+    end
+  end
+
+  def self.down
+    remove_column :core_demographics, :ethnicity, :string
+    remove_column :core_demographics, :religion, :string
   end
 end
