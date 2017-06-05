240a4f2784bf6a5ce0f32dde6dc104ba275a6249
bilel <bilel.kedidi@gmail.com>
2017-06-05 14:30:51 +0100

    fix

--- app/controllers/exception_notifier_controller.rb
+++
@@ -1,9 +0,0 @@
-class ExceptionNotifierController < ApplicationController
-  def index
-    @exceptions = ExceptionNotifier.where('created_at > ?', 7.days.ago).order('created_at DESC')
-  end
-
-  def show
-    @exception = ExceptionNotifier.find params[:id]
-  end
-end
--- app/models/exception_notifier.rb
+++
@@ -1,7 +0,0 @@
-class ExceptionNotifier < ApplicationRecord
-  def self.create(e)
-    super(title: e.message, content: e.backtrace)
-  rescue
-    puts 'error on creating'
-  end
-end
