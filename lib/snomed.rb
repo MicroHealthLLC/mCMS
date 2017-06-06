f6f31df5c78a193136f2e2bb0313d72869c8d020
bilel <bilel.kedidi@gmail.com>
2017-06-06 13:10:12 +0100

    improve snomed search

--- lib/snomed.rb
+++ lib/snomed.rb
@@ -3,7 +3,7 @@ module Snomed
     redis = Redis.new
     tgt = redis.get('tgt_token')
     unless tgt
-     tgt = create_tgt
+      tgt = create_tgt
     end
     tgt
   end
@@ -29,32 +29,65 @@ module Snomed
     res.body
   end
 
-  def get_snomed_result(term, default_search, st_ticket = get_st)
-    retries = 0
+  def get_snomed_result(term, default_search, options = {} )
+    json = get_json(term, default_search, options)
     data = []
+
+    if json['result']['results'].first['name'] == 'NO RESULTS'
+      json = get_json(term, default_search, options.reverse_merge({searchType: 'approximate', pageSize: 200 }))
+      if json['result']['results']
+        results = json['result']['results'].select{|v| v['name'].include?(term) }
+        data = results.map do |d|
+          {id: d['name'], name: d['name'], label: d['name'] }
+        end
+        if data.empty?
+          data = [{id: 'NO RESULTS', name: 'NO RESULTS', label: 'NO RESULTS' }]
+        end
+      end
+    # data = get_snomed_from_ihtsdotools(term, default_search)
+    elsif json['result']['results']
+      data = json['result']['results'].map do |d|
+        {id: d['name'], name: d['name'], label: d['name'] }
+      end
+    end
+
+    data
+  end
+
+  # def get_snomed_from_ihtsdotools(term, default_search)
+  #   url = 'http://browser.ihtsdotools.org/api/snomed/en-edition/v20170131/descriptions'
+  #   res = HTTParty.get(url,
+  #                      query: {
+  #                          query: "#{term} #{default_search}",
+  #                          limit: 50,
+  #                          searchMode: 'partialMatching'
+  #                      })
+  #   json = JSON.parse res.body
+  #   json['matches'].map do |d|
+  #     {id: d['fsn'], name: d['fsn'], label: d['fsn'] }
+  #   end
+  # end
+
+  def get_json(term, default_search, options = {} )
+    st_ticket = get_st
+    retries = 0
+    json = {}
     ticket = st_ticket
     begin
       res = HTTParty.get("https://uts-ws.nlm.nih.gov/rest/search/current",
                          query: {
                              ticket: ticket,
                              string: "#{term} #{default_search}"
-                         } )
+                         }.merge(options) )
       json = JSON.parse res.body
-      data = []
-      if json['result']['results']
-        data = json['result']['results'].map do |d|
-          {id: d['name'], name: d['name'], label: d['name'] }
-        end
-      end
     rescue
       if retries == 0
         create_tgt
         retries += 1
         retry
       end
-
     end
-    data
+    json
   end
 
   # def check_validity_ticket(ticket)
