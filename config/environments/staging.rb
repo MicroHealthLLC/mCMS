f6a78d444f3fe299082ed1f444d89a75ba1dd9dc
bilel <bilel.kedidi@gmail.com>
2017-06-07 11:49:35 +0100

    make exception notifier on prod and staging only

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -96,6 +96,13 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  # Exception Notifier Gem
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+                                          :email => {
+                                              :exception_recipients => %w{bilel.kedidi@gmail.com}
+                                          }
+
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
