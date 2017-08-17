018316a90a71ab4b7b36cb424c404a790b231771
bilel <bilel.kedidi@gmail.com>
2017-08-17 22:35:12 +0100

    make devise/edit using tabs

--- app/helpers/cases_helper.rb
+++ app/helpers/cases_helper.rb
@@ -1,7 +1,7 @@
 module CasesHelper
   def show_relation_path(relation)
     if relation.relation.is_a?(Survey::Survey)
-       link_to(relation.relation, surveys_path(relation.relation) )
+      link_to(relation.relation, surveys_path(relation.relation) )
     else
       link_to(relation.relation, relation.relation )
     end
@@ -12,27 +12,83 @@ module CasesHelper
   end
 
   def case_tabs
-    tabs =  [
-        {:name => 'subcases', :partial => 'cases/show_case/subcases', :label => :subcases},
-        {:name => 'case_support', :partial => 'cases/show_case/case_support', :label => :case_support},
-        {:name => 'notes', :partial => 'cases/show_case/notes', :label => :notes},
-        {:name => 'needs', :partial => 'cases/show_case/needs', :label => :needs},
-        {:name => 'goals', :partial => 'cases/show_case/goals', :label => :goals},
-        {:name => 'plans', :partial => 'cases/show_case/plans', :label => :plans},
-        {:name => 'tasks', :partial => 'cases/show_case/tasks', :label => :tasks},
-        {:name => 'signatures', :partial => 'cases/show_case/jsignatures', :label => :signatures},
-        {:name => 'documents', :partial => 'cases/show_case/documents', :label => :documents},
-        {:name => 'enrollments', :partial => 'cases/show_case/enrollments', :label => :enrollments},
-        {:name => 'teleconsults', :partial => 'cases/show_case/teleconsults', :label => :teleconsults},
-        {:name => 'checklists', :partial => 'cases/show_case/checklists', :label => :checklists},
-        {:name => 'transports', :partial => 'cases/show_case/transports', :label => :transports},
-        {:name => 'measurement_records', :partial => 'cases/show_case/measurement_records', :label => :measurement_records},
-        {:name => 'appointments', :partial => 'cases/show_case/appointments', :label => :appointments},
-        {:name => 'referrals', :partial => 'cases/show_case/referrals', :label => :referrals},
-        {:name => 'surveys', :partial => 'cases/show_case/surveys', :label => :surveys},
-        {:name => 'case_organizations', :partial => 'cases/show_case/case_organizations', :label => :case_organizations},
-        {:name => 'watcher', :partial => 'cases/show_case/watcher', :label => :watcher},
-    ]
+    tabs =  [ ]
+    if @cases
+      tabs<<  {:name => 'subcases', :partial => 'cases/show_case/subcases', :label => :subcases}
+    end
+
+    if @case_supports
+      tabs<< {:name => 'case_support', :partial => 'cases/show_case/case_support', :label => :case_support}
+    end
+
+    if @notes
+      tabs<<  {:name => 'notes', :partial => 'cases/show_case/notes', :label => :notes}
+    end
+
+    if @needs
+      tabs<<  {:name => 'needs', :partial => 'cases/show_case/needs', :label => :needs}
+    end
+
+    if @goals
+      tabs<<  {:name => 'goals', :partial => 'cases/show_case/goals', :label => :goals}
+    end
+
+    if @plans
+      tabs<< {:name => 'plans', :partial => 'cases/show_case/plans', :label => :plans}
+    end
+
+    if @tasks
+      tabs<< {:name => 'tasks', :partial => 'cases/show_case/tasks', :label => :tasks}
+    end
+
+    if @jsignatures
+      tabs<<  {:name => 'signatures', :partial => 'cases/show_case/jsignatures', :label => :signatures}
+    end
+
+    if @documents
+      tabs<< {:name => 'documents', :partial => 'cases/show_case/documents', :label => :documents}
+    end
+
+    if @enrollments
+      tabs<< {:name => 'enrollments', :partial => 'cases/show_case/enrollments', :label => :enrollments}
+    end
+
+    if @teleconsults
+      tabs<< {:name => 'teleconsults', :partial => 'cases/show_case/teleconsults', :label => :teleconsults}
+    end
+
+    if @checklists
+      tabs<< {:name => 'checklists', :partial => 'cases/show_case/checklists', :label => :checklists}
+    end
+
+    if @transports
+      tabs<< {:name => 'transports', :partial => 'cases/show_case/transports', :label => :transports}
+    end
+
+    if @measurement_records
+      tabs<< {:name => 'measurement_records', :partial => 'cases/show_case/measurement_records', :label => :measurement_records}
+    end
+
+    if @appointments
+      tabs<< {:name => 'appointments', :partial => 'cases/show_case/appointments', :label => :appointments}
+    end
+
+    if @referrals
+      tabs<<    {:name => 'referrals', :partial => 'cases/show_case/referrals', :label => :referrals}
+    end
+
+    if @surveys
+      tabs<< {:name => 'surveys', :partial => 'cases/show_case/surveys', :label => :surveys}
+    end
+
+    if @case_organizations
+      tabs<<  {:name => 'case_organizations', :partial => 'cases/show_case/case_organizations', :label => :case_organizations}
+    end
+
+    if @watchers
+      tabs<< {:name => 'watcher', :partial => 'cases/show_case/watcher', :label => :watcher}
+    end
+
     tabs
   end
 
