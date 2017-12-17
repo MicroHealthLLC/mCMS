a49d0232fdce2d6e12cddcc76d6f4046aaf1741c
bilel-kedidi <bilel.kedidi@gmail.com>
2017-12-17 21:22:51 +0100

    fix user uuid

--- db/migrate/20171216194454_add_uuid_to_user.rb
+++ db/migrate/20171216194454_add_uuid_to_user.rb
@@ -1,7 +1,7 @@
 class AddUuidToUser < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :uuid, :string
-    User.all.each do |user|
+    User.unscoped.each do |user|
       user.uuid = 10_000_000_000 + user.id
       user.save
     end
