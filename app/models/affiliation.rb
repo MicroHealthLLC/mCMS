e82f84ae69d375c6b8184c9047fd54fa3037a4a7
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-02-02 13:53:14 -0500

    Update affiliation.rb

--- app/models/affiliation.rb
+++ app/models/affiliation.rb
@@ -11,6 +11,11 @@ class Affiliation < ApplicationRecord
   accepts_nested_attributes_for :affiliation_attachments, reject_if: :all_blank, allow_destroy: true
 
   validates_presence_of :name, :user_id
+  before_validation do
+    if self.date_end.present? and self.date_start.present? and self.date_start > self.date_end
+      errors[:base] << "End date cannot be earlier than start date"
+    end
+  end
 
   def self.enumeration_columns
     [
