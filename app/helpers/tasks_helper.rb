f254ca156b61ac8d9dc89718df0328412bb90850
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-02-05 12:42:59 -0500

    Update tasks_helper.rb

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,7 +1,11 @@
 module TasksHelper
   def get_back_url task
     if task.related_to_id
-      return case_path(task.related_to_id) if task.related_to_type == 'case'
+      if task.related_to_type == 'case'
+        return case_path(task.related_to_id)
+      else
+        return task_path(task.related_to_id)
+      end
     end
     tasks_path
   end
