6f94b8f48518cc2ccc264a26b25073df6eada53a
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-02-09 14:12:31 -0500

    Update cases_controller.rb

--- app/controllers/cases/cases_controller.rb
+++ app/controllers/cases/cases_controller.rb
@@ -99,7 +99,7 @@ class CasesController < UserCasesController
     @timeline << @case.transports
     @timeline << @case.case_supports.active
     @timeline << @case.worker_compensations
-    @timeline << @case.job_apps.active
+    @timeline << @case.job_apps
     @timeline.flatten!.compact!
     @timeline.sort_by!{|a| Time.now - a.updated_at }
     respond_to do |format|
