f68c6964c9f89fdd7897cee4e44e159a3b36aa0c
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-02-26 15:58:21 -0500

    Update checklists_controller.rb

--- app/controllers/admin/checklists_controller.rb
+++ app/controllers/admin/checklists_controller.rb
@@ -69,7 +69,7 @@ class ChecklistsController < ProtectForgeryApplication
     else
       flash[:error] = @checklist.errors.full_messages.join('<br/>')
       respond_to do |format|
-        format.html { redirect_to edit_checklist_templates_url(@checklist) }
+        format.html { redirect_to edit_checklist_template_url(@checklist) }
         format.json { head :no_content }
       end
     end      
