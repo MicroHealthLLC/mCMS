b41d54abe41305ed83227b18978722ed860c2092
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-02-28 12:00:52 -0500

    Update user_profiles_helper.rb

--- app/helpers/user_profiles_helper.rb
+++ app/helpers/user_profiles_helper.rb
@@ -3,10 +3,9 @@ module UserProfilesHelper
     tabs = [
         {:name => 'core_demographic', :partial => 'user_profiles/profile_record/core_demography', :label => :core_demography},
         {:name => 'extend_demographic', :partial => 'user_profiles/profile_record/extend_demography', :label => :extend_demography},
-        {:name => 'related_client', :partial => 'user_profiles/profile_record/related_client', :label => 'Related Clients'},
-        {:name => 'identification', :partial => 'user_profiles/profile_record/identification', :label => 'Identifications'}
+        {:name => 'related_client', :partial => 'user_profiles/profile_record/related_client', :label => 'Related Clients'}
     ]
-    # tabs << {:name => 'identification', :partial => 'devise/registrations/shared/identification_form', :label => 'Identifications'}
+    tabs << {:name => 'identification', :partial => 'devise/registrations/shared/identification_form', :label => 'Identifications'}
 
     if @languages
       tabs<<  {:name => 'languages', :partial => 'user_profiles/profile_record/languages', :label => :languages}
