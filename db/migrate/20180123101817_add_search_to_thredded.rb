b47b5490cc5ea40f29d7d9f979a23d3d3e3726f6
bilel-kedidi <bilel.kedidi@gmail.com>
2018-03-10 10:08:55 +0100

    fix index exist

--- db/migrate/20180123101817_add_search_to_thredded.rb
+++ db/migrate/20180123101817_add_search_to_thredded.rb
@@ -1,6 +1,10 @@
 class AddSearchToThredded < ActiveRecord::Migration[5.0]
   def change
-    DbTextSearch::FullText.add_index connection, :thredded_topics, :title, name: :thredded_topics_title_fts
-    DbTextSearch::FullText.add_index connection, :thredded_posts, :content, name: :thredded_posts_content_fts
+    unless index_exists?(:thredded_topics, :title, name: :thredded_topics_title_fts)
+      DbTextSearch::FullText.add_index connection, :thredded_topics, :title, name: :thredded_topics_title_fts
+    end
+    unless index_exists?(:thredded_topics, :title, name: :thredded_topics_title_fts)
+      DbTextSearch::FullText.add_index connection, :thredded_posts, :content, name: :thredded_posts_content_fts
+    end
   end
 end
