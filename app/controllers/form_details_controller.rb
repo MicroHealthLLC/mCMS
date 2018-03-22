e12e250daaf76c62c83ff24f08fd49f988c9ddbf
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-03-22 12:28:13 -0400

    Update form_details_controller.rb

--- app/controllers/form_details_controller.rb
+++ app/controllers/form_details_controller.rb
@@ -107,10 +107,10 @@ class FormDetailsController < ApplicationController
 
   def back_url
     case @formular.placement.to_i
-      when 1 then '/profile_record'
-      when 2 then '/occupation_record'
-      when 3 then '/medical_record'
-      when 4 then '/socioeconomic_record'
+      when 1 then '/profile_record#tabs-forms'
+      when 2 then '/occupation_record#tabs-forms'
+      when 3 then '/medical_record#tabs-forms'
+      when 4 then '/socioeconomic_record#tabs-forms'
       when 5 then '/cases'
         else root_path
     end
